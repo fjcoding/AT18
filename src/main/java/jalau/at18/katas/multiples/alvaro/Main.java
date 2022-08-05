6a7e3f89649310e575ccd9a57c2570f771454d43
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-05 12:37:06 -0400

    Added solution of excersise multiples of kata and modified file build gradle with task

--- src/main/java/jalau/at18/katas/multiples/alvaro/Main.java
+++ src/main/java/jalau/at18/katas/multiples/alvaro/Main.java
@@ -1,5 +1,19 @@
 package jalau.at18.katas.multiples.alvaro;
 
 public class Main {
-
+    public static void main(String[] args) {
+    int input = 10;
+    int added = 0;
+    for(int ind=0;ind<input;ind++){
+        if(ind%3 ==0 || ind%5==0 ){
+            added = added + ind;
+        }
+    }
+    if(input<0){
+        System.out.println("0");
+    }
+    else{
+        System.out.println("La suma es: "+added);
+    }
+    }
 }

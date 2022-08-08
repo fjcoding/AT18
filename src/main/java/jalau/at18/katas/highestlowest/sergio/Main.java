d62de5e36f9c90af0fbe887515ee04ce9f170aae
Sergio Lema <sergio.lema@jala.university>
2022-08-07 23:29:57 -0400

    highestlowest file homework

---
+++ src/main/java/jalau/at18/katas/highestlowest/sergio/Main.java
@@ -0,0 +1,31 @@
+package main.java.jalau.at18.katas.highestlowest.sergio;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] input = new String[] { "1", "2", "3" };
+        int[] highestandLowest = HighestLowest(input);
+        System.out.println(highestandLowest[0] + " " + highestandLowest[1]);
+    }
+
+    public static int[] HighestLowest(String[] input) {
+        int[] values = Arrays.stream(input)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int max = values[0];
+        int min = values[0];
+        for (int number : values) {
+            if (number > max) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        int[] result = new int[] { max, min };
+        return result;
+    }
+
+}

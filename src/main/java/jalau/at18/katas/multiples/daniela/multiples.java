84fc047a2a796541ae73bf7fcb3c65d06b5ae294
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-08 09:17:37 -0400

    Add likes, multiples katas

---
+++ src/main/java/jalau/at18/katas/multiples/daniela/multiples.java
@@ -0,0 +1,25 @@
+package jalau.at18.katas.multiples.daniela;
+
+public class multiples {
+    static void multiplesfuntion(int number) {
+        if (number < 0) {
+            System.out.println("The number is not valid");
+        }
+        int sumatotal = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                // int sumatotal = 0;
+                sumatotal += i;
+                // System.out.println("La suma total es:" + sumatotal);
+            }
+
+        }
+        System.out.println("La suma total es:" + sumatotal);
+
+    }
+
+    public static void main(String[] args) {
+        multiplesfuntion(100);
+    }
+
+}

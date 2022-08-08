33c1945d9b0015892f9751fea998b3edc2875b00
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-08 09:04:17 -0400

    Highest and lowest number, add a folder as libertad and a file with my solution

---
+++ src/main/java/jalau/at18/katas/highestlowest/libertad/Main.java
@@ -0,0 +1,27 @@
+package jalau.at18.katas.highestlowest.libertad;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int menor,mayor;
+        mayor=-1000;
+        menor=1000;
+        System.out.println("Introdusca los numeros:");
+        String datos= scanner.nextLine();
+        String[] numeros = datos.split(" ");
+        int i=0;
+        while(i<numeros.length){
+            int numero= Integer.parseInt(numeros[i]);
+            if(numero>mayor){
+                mayor=numero;
+            }
+            if(numero<menor){
+                menor=numero;
+            }
+            i++;
+        }
+        System.out.println(mayor+" "+menor);
+    }
+}

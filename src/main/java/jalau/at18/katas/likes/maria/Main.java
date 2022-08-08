29259b6ceb2659bfd1607f0783819fef430e5e3e
Maria Hurtado <Maria.Hurtado@jala.university>
2022-08-08 15:35:20 -0400

    Changing the position brackets

--- src/main/java/jalau/at18/katas/likes/maria/Main.java
+++ src/main/java/jalau/at18/katas/likes/maria/Main.java
@@ -2,12 +2,12 @@ package jalau.at18.katas.likes.maria;
 
 public class Main {
     public static void main(String[] args) {
-        String people1[] = {};//-->  "no one likes this"
-        String people2[] = {"Peter"};//-->  "Peter likes this"
-        String people3[] = {"Jacob", "Alex"};//-->  "Jacob and Alex like this"
-        String people4[] = {"Max", "John", "Mark"};//"Max, John and Mark like this"
-        String people5[] = {"Alex", "Jacob", "Mark", "Max"};//-->  "Alex, Jacob and 2 others like this"
-        String people6[] = {"Karina", "Elsa", "Marcela", "Silvia","Oscar","Vladimir","Romina"};
+        String [] people1 = {};//-->  "no one likes this"
+        String [] people2 = {"Peter"};//-->  "Peter likes this"
+        String [] people3 = {"Jacob", "Alex"};//-->  "Jacob and Alex like this"
+        String [] people4 = {"Max", "John", "Mark"};//"Max, John and Mark like this"
+        String [] people5 = {"Alex", "Jacob", "Mark", "Max"};//-->  "Alex, Jacob and 2 others like this"
+        String [] people6= {"Karina", "Elsa", "Marcela", "Silvia","Oscar","Vladimir","Romina"};
 
         System.out.println(likeAnswer(people1));
         System.out.println(likeAnswer(people2));
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(likeAnswer(people5));
         System.out.println(likeAnswer(people6));
     }
-    public static String likeAnswer(String []namesList){
+    public static String likeAnswer(String [] namesList){
         String message = "";
         int numberPeople = namesList.length;
         if(numberPeople == 0){

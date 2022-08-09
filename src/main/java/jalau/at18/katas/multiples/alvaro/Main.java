f3a55eb575330a3c7d399610508ba474ebc64580
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-09 16:31:52 -0400

    Refactor multiples excersise and added Junit test

--- src/main/java/jalau/at18/katas/multiples/alvaro/Main.java
+++ src/main/java/jalau/at18/katas/multiples/alvaro/Main.java
@@ -5,17 +5,33 @@ public class Main {
     private static final int DIVIDER_FIVE = 5;
     public static final int NUMBER_TEST = 10;
     public static void main(String[] args) {
+        int added;
+        Main main = new Main();
+        if (main.numberValid(NUMBER_TEST)) {
+            added = main.added(NUMBER_TEST);
+            System.out.println("The Added is " + added);
+        }
+
+
+    }
+    public boolean numberValid(int numberTest) {
+        if (numberTest < 0) {
+            System.out.println("0");
+            return false;
+        } else {
+            return true;
+
+        }
+    }
+    public int added(int numberTest) {
 
         int added = 0;
-        for (int ind = 0; ind < NUMBER_TEST; ind++) {
+        for (int ind = 0; ind < numberTest; ind++) {
             if (ind % DIVIDER_THREE == 0 || ind % DIVIDER_FIVE == 0) {
                 added = added + ind;
             }
         }
-        if (NUMBER_TEST < 0) {
-            System.out.println("0");
-        } else {
-            System.out.println("La suma es: " + added);
-        }
+
+        return added;
     }
 }

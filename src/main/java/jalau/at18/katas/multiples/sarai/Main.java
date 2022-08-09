96d73fce2a70ed50b4ec96213e808b62b2d3553e
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-08 20:52:39 -0400

    Correct code with code conventions - checkstyle of exercice multiples

--- src/main/java/jalau/at18/katas/multiples/sarai/Main.java
+++ src/main/java/jalau/at18/katas/multiples/sarai/Main.java
@@ -1,31 +1,24 @@
 package jalau.at18.katas.multiples.sarai;
 import java.util.Scanner;
 public class Main {
+    static final int MODULOS_TRES = 3;
+    static final int MODULO_CINCO = 5;
     public static void main(String[] args) {
-
-        int num, suma=0;
-        Scanner pp=new Scanner(System.in);
+        int num;
+        int suma = 0;
+        Scanner numero_scanner = new Scanner(System.in);
         System.out.println("Enter a number ");
-        num=pp.nextInt();
-       if(num>0)
-       {
-           for (int i = 0; i < num; i++)
-           {
-               if (i % 3 == 0 || i % 5 == 0)
-               {
-                  System.out.println(i);
-                   suma += i;
-               }
-           }
-          System.out.println("the sum of the numbers is: " + suma);
-       }
-       
-       else
-           System.out.println("the sum of the numbers is: 0 ");
-
-
-       
-
-
+        num = numero_scanner.nextInt();
+        if (num > 0) {
+            for (int index = 0; index < num; index++) {
+                if (index % MODULOS_TRES == 0 || index % MODULO_CINCO == 0) {
+                    System.out.println(index);
+                    suma += index;
+                }
+            }
+            System.out.println("the sum of the numbers is: " + suma);
+        } else {
+            System.out.println("the sum of the numbers is: 0 ");
+        }
     }
 }

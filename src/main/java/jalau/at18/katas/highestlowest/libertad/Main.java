8efc76131af4d51492e8359d0fa1430828850eaa
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-08 22:01:40 -0400

    troubleshoot checkstyle problems in highestlowest/libertad/Main direction

--- src/main/java/jalau/at18/katas/highestlowest/libertad/Main.java
+++ src/main/java/jalau/at18/katas/highestlowest/libertad/Main.java
@@ -3,26 +3,25 @@ package jalau.at18.katas.highestlowest.libertad;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int menor,mayor;
-        mayor=-1000;
-        menor=1000;
         System.out.println("Introdusca los numeros:");
-        String datos= scan.nextLine();
+        String datos = scan.nextLine();
         String[] numeros = datos.split(" ");
-        int i=0;
-        while(i<numeros.length){
-            int numero= Integer.parseInt(numeros[i]);
-            if(numero>mayor){
-                mayor=numero;
+        int iterator = 0;
+        int mayor = Integer.parseInt(numeros[iterator]);
+        int menor = Integer.parseInt(numeros[iterator]);
+        while (iterator < numeros.length) {
+            int numero = Integer.parseInt(numeros[iterator]);
+            if (numero > mayor) {
+                mayor = numero;
             }
-            if(numero<menor){
-                menor=numero;
+            if (numero < menor) {
+                menor = numero;
             }
-            i++;
+            iterator++;
         }
-        System.out.println(mayor+" "+menor);
+        System.out.println(mayor + " " + menor);
         scan.close();
     }
 }

e1c53743d716b0f2115d6329e058dbbb0b9c4eaa
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-08 20:51:53 -0400

    Correct code with code conventions - checkstyle of exercice likes

--- src/main/java/jalau/at18/katas/likes/sarai/Main.java
+++ src/main/java/jalau/at18/katas/likes/sarai/Main.java
@@ -1,65 +1,41 @@
 package jalau.at18.katas.likes.sarai;
 import java.util.Scanner;
 public class Main {
+    static final int MAS_DE_CUATRO_NOMBRES = 4;
+    static final int TRES_PERSONAS = 3;
     public static void main(String[] args) {
-       
-        
         String nombres;
-        Scanner pp=new Scanner(System.in);
+        Scanner palabra_scanner = new Scanner(System.in);
         System.out.println("enter the size of the array");
-        int tam=pp.nextInt();
- 
-        if(tam>=1)
-        {
+        int tam = palabra_scanner.nextInt();
+        if (tam >= 1) {
             String[] personas = new String[tam];
             System.out.println("Enter names");
-               for (int i = 0; i < tam; i++)
-               {
-                   nombres = pp.next();
-                   personas[i] = nombres;
-                   if (tam == 1)
-                   {
-                       System.out.println(personas[i] + " likes this");
-                   }
-               }
-
-              
-
-              if (tam >1 && tam < 4 )
-              {
-                  for(int i = 0; i < personas.length; i++)//imprimir los nombres del array
-                  {  
-                      System.out.print(personas[i] + ",");
-                  }
-
-                  System.out.print(" like this");
-
-              }
-
-
-               if(tam >= 4)
-              { 
-                      for (int i = 0; i < 2 ; i++)//imprimir los nombres del array
-                      {
-                           System.out.print(personas[i] + ",");
-                      }
-                      System.out.print(personas[2]);
-
-                  int ress = tam - 3;
-                  System.out.print(" and "+ ress +" others like this");
-              }
+            for (int index = 0; index < tam; index++) {
+                nombres = palabra_scanner.next();
+                personas[index] = nombres;
+                if (tam == 1) {
+                    System.out.println(personas[index] + " likes this");
+                }
+            }
+            if (tam > 1 && tam < MAS_DE_CUATRO_NOMBRES) {
+                for (int index = 0; index < personas.length; index++) {
+                    System.out.print(personas[index] + ",");
+                }
+                System.out.print(" like this");
+            }
+            if (tam >= MAS_DE_CUATRO_NOMBRES) {
+                for (int index = 0; index < 2; index++) {
+                    System.out.print(personas[index] + ",");
+                }
+                System.out.print(personas[2]);
+                int ress = tam - TRES_PERSONAS;
+                System.out.print(" and " + ress + " others like this");
+            }
+        } else {
+            if (tam == 0 || tam < 1) {
+                System.out.println("no one likes this");
+            }
         }
-        else
-        {
-        
-         if (tam == 0 || tam<1)
-           {
-               System.out.println("no one likes this");
-           }
-        }
-
-
-
-
     }
 }

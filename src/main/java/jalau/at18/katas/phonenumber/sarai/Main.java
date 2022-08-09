ddf53877865d62e8497eae769cb990996c8d8f8b
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-08 20:53:13 -0400

    Correct code with code conventions - checkstyle of exercice phonenumber

--- src/main/java/jalau/at18/katas/phonenumber/sarai/Main.java
+++ src/main/java/jalau/at18/katas/phonenumber/sarai/Main.java
@@ -2,31 +2,30 @@ package jalau.at18.katas.phonenumber.sarai;
 import java.util.Scanner;
 
 public class Main {
+    static final int POSICION_CERO = 0;
+    static final int POSICION_TRES = 3;
+    static final int POSICION_SEIS = 6;
+    static final int CONTT = 10;
     public static void main(String[] args) {
-        Scanner pp=new Scanner(System.in);
+        Scanner numero_scanner = new Scanner(System.in);
         int num;
         System.out.println("Enter 10 numbers");
-        int contt = 10;
-        int[] almac = new int[10];
-        for (int i = 0; i < contt; i++)
-        {    num=pp.nextInt();
-             almac[i] = num;
+        int[] almac = new int[CONTT];
+        for (int index = 0; index < CONTT; index++) {
+            num = numero_scanner.nextInt();
+            almac[index] = num;
         }
-
-        for(int i = 0; i < 10;i++){ 
-            if(i==0)
-            {
-               System.out.print("(");
+        for (int index = 0; index < CONTT; index++) {
+            if (index == POSICION_CERO) {
+                System.out.print("(");
             }
-            if (i == 3)
-            {
+            if (index == POSICION_TRES) {
                 System.out.print(")");
             }
-            if (i==6)
-            {
+            if (index == POSICION_SEIS) {
                 System.out.print("-");
             }
-            System.out.print(almac[i]);
+            System.out.print(almac[index]);
         }
     }
 }

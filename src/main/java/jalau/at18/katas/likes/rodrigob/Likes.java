4e237b183191d90311504381c8896b0be18afb55
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-08-09 16:34:36 -0400

    Add Unit Tests for the Likes kata

--- src/main/java/jalau/at18/katas/likes/rodrigob/Likes.java
+++ src/main/java/jalau/at18/katas/likes/rodrigob/Likes.java
@@ -15,13 +15,13 @@ public class Likes {
                 out = names[0] + " likes this";
                 break;
             case 2:
-                out = names[0] + " and " + names[1] + " likes this";
+                out = names[0] + " and " + names[1] + " like this";
                 break;
             case option:
-                out = names[0] + ", " + names[1] + " and " + names[2] + " likes this";
+                out = names[0] + ", " + names[1] + " and " + names[2] + " like this";
                 break;
             default:
-                out = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others likes this";
+                out = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
                 break;
         }
         return out;

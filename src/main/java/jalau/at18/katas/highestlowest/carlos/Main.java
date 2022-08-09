918c892c2dd8405650786c46254f873a96599afd
Carlos Calderon <Carlos.Calderon@Jala.University>
2022-08-08 21:52:42 -0400

    prueba2

---
+++ src/main/java/jalau/at18/katas/highestlowest/carlos/Main.java
@@ -0,0 +1,24 @@
+package jalau.at18.katas.highestlowest.carlos;
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args ) {
+        String texto=JOptionPane.showInputDialog("Numeros:");
+        String numEnTex[]=texto.split(" ");
+        int n=numEnTex.length;
+        int numero[]=new int [n];
+        for (int i=0; i<n;i++){
+            numero[i]=Integer.parseInt(numEnTex[i]);
+        }
+        int mayor=numero[0];
+        int menor=numero[0];
+        for(int i=0;i<n;i++){
+           if (numero[i]>mayor){
+               mayor=numero[i];
+           }
+           if(numero[i]<menor){
+               menor=numero[i];
+           }
+        }
+        System.out.println(mayor+" "+menor);
+    }
+}

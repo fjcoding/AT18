1003da4dfc95eb0b409febec5dda8fc55744e3f4
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-08 20:51:17 -0400

    Correct code with code conventions - checkstyle of exercice highestlowest

--- src/main/java/jalau/at18/katas/highestlowest/sarai/Main.java
+++ src/main/java/jalau/at18/katas/highestlowest/sarai/Main.java
@@ -4,35 +4,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int cant;
-         Scanner pp=new Scanner(System.in);
-         System.out.println("How many numbers do you want to enter?");
-         cant=pp.nextInt();
-         int[] alma = new int[cant];
-
+        Scanner scanner_teclado = new Scanner(System.in);
+        System.out.println("how many numbers do you want to enter?");
+        cant = scanner_teclado.nextInt();
+        int[] alma = new int[cant];
         System.out.println("enter numbers");
-        for (int i = 0; i<cant; i++)
-        {
-            int num = pp.nextInt();
-            alma[i] = num;
+        for (int index = 0; index < cant; index++) {
+            int num = scanner_teclado.nextInt();
+            alma[index] = num;
         }
         int numMayor = alma[0];
         int numMenor = alma[0];
-        for (int i = 0; i <alma.length; i++)
-        {
-                if (alma[i] > numMayor)
-                {
-                    numMayor = alma[i];
-                }
-                if (alma[i] < numMenor)
-                {
-                    numMenor = alma[i];
-                }
+        for (int index = 0; index < alma.length; index++) {
+            if (alma[index] > numMayor) {
+                numMayor = alma[index];
+            }
+            if (alma[index] < numMenor) {
+                numMenor = alma[index];
+            }
         }
-     System.out.println("numbers "+ numMayor+ " " + numMenor);
-
-
-
-
-
+        System.out.println("numbers " + numMayor + " " + numMenor);
     }
 }

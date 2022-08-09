4e5a0ebd53862d55cdedbecf8684871abb20ec5b
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-08-09 10:52:46 -0400

    Add solution for the likes kata

---
+++ src/main/java/jalau/at18/katas/likes/rodrigob/Likes.java
@@ -0,0 +1,29 @@
+package jalau.at18.katas.likes.rodrigob;
+public class Likes {
+
+    public static void main(String[] args) {
+        System.out.print(whoLikes(new String[] {"Valeria", "Alex", "Andres", "Jacob", "Mark", "Max"}));
+    }
+    public static String whoLikes(String[] names) {
+        String out = "";
+        final int option = 3;
+        switch (names.length) {
+            case 0:
+                out = "no one likes this";
+                break;
+            case 1:
+                out = names[0] + " likes this";
+                break;
+            case 2:
+                out = names[0] + " and " + names[1] + " likes this";
+                break;
+            case option:
+                out = names[0] + ", " + names[1] + " and " + names[2] + " likes this";
+                break;
+            default:
+                out = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others likes this";
+                break;
+        }
+        return out;
+    }
+}

ba3852dce760ca590cdb73718e4b4fef044e500f
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-09 22:49:00 -0400

    Modify and clear my code of Multiples

--- src/main/java/jalau/at18/katas/multiples/daniela/Main.java
+++ src/main/java/jalau/at18/katas/multiples/daniela/Main.java
@@ -1,14 +1,11 @@
 package jalau.at18.katas.multiples.daniela;
-import java.util.Scanner;
+
 public class Main {
     static final Integer DIVIDE_1 = 3;
     static final Integer DIVIDE_2 = 5;
     public static void main(String[] args) {
-        System.out.print("Enter a number: ");
-        Scanner scan = new Scanner(System.in);
-        Integer number = scan.nextInt();
-        Integer sumMultiples = getSumMult(number);
-        System.out.println("Sum: " + sumMultiples);
+        final Integer number = 100;
+        getSumMult(number);
     }
     public static Integer getSumMult(Integer number) {
         Integer sum = 0;

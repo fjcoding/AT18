20c8e2becc21316c3c3255c6967fbf0bf95f02da
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-10 09:07:07 -0400

    add Main file in the team3 folder

---
+++ src/main/java/jalau/at18/katas/2048/team3/Main.java
@@ -0,0 +1,24 @@
+package jalau.at18.katas.2048.team3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] matrix = {{0,0,0,2},{0,0,2,2},{0,0,0,0},{0,0,0,0}};
+
+    }
+
+    public static void rightMove(int[][] matrix) {
+
+    }
+    public static void downMove(int[][] matrix) {
+
+    }
+    public static void leftMove(int[][] matrix) {
+
+    }
+    public static void upMove(int[][] matrix) {
+
+    }
+}

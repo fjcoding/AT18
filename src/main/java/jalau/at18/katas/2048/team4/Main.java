c1fc2a9d3c3876ed954b2dddb3da979ca9115581
Rene Rengel <Rene.Rengel@jala.university>
2022-08-10 09:38:32 -0400

    Fix function placement

--- src/main/java/jalau/at18/katas/2048/team4/Main.java
+++ src/main/java/jalau/at18/katas/2048/team4/Main.java
@@ -11,34 +11,33 @@ public class Main {
         int randomNum2 = ThreadLocalRandom.current().nextInt(min, max + 1);
         //System.out.println(randomNum);
 
-    static int[][] GeneratingAMatrix(int randomNum1, int randomNum2) {
-        int matriz[][] = new int[4][4];
-        matriz[0][0] = 0;
-        matriz[0][1] = 0;
-        matriz[0][2] = 0;
-        matriz[0][3] = 0;
-        matriz[1][0] = 0;
-        matriz[1][1] = 0;
-        matriz[1][2] = 0;
-        matriz[1][3] = 0;
-        matriz[2][0] = 0;
-        matriz[2][1] = 0;
-        matriz[2][2] = 0;
-        matriz[2][3] = 0;
-        matriz[3][0] = 0;
-        matriz[3][1] = 0;
-        matriz[3][2] = 0;
-        matriz[3][3] = 0;
-
-        matriz [randomNum1][randomNum2]= 2;
-        matriz [randomNum1][randomNum2]= 4;
-
-        return matriz;
-
-    }
+   
 
 
+}
+static int[][] GeneratingAMatrix(int randomNum1, int randomNum2) {
+    int matriz[][] = new int[4][4];
+    matriz[0][0] = 0;
+    matriz[0][1] = 0;
+    matriz[0][2] = 0;
+    matriz[0][3] = 0;
+    matriz[1][0] = 0;
+    matriz[1][1] = 0;
+    matriz[1][2] = 0;
+    matriz[1][3] = 0;
+    matriz[2][0] = 0;
+    matriz[2][1] = 0;
+    matriz[2][2] = 0;
+    matriz[2][3] = 0;
+    matriz[3][0] = 0;
+    matriz[3][1] = 0;
+    matriz[3][2] = 0;
+    matriz[3][3] = 0;
 
+    matriz [randomNum1][randomNum2]= 2;
+    matriz [randomNum1][randomNum2]= 4;
+
+    return matriz;
 
 }
 

970b64f6629e6e42731040834e2e4ea314320a9c
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-09 21:44:06 -0400

    modified code to english,restructured and unit test to likes files

--- src/main/java/jalau/at18/katas/multiples/sarai/Main.java
+++ src/main/java/jalau/at18/katas/multiples/sarai/Main.java
@@ -1,24 +1,30 @@
 package jalau.at18.katas.multiples.sarai;
 import java.util.Scanner;
 public class Main {
-    static final int MODULOS_TRES = 3;
-    static final int MODULO_CINCO = 5;
+    static final int THREE_MODULE = 3;
+    static final int FIVE_MODULE = 5;
     public static void main(String[] args) {
-        int num;
-        int suma = 0;
-        Scanner numero_scanner = new Scanner(System.in);
         System.out.println("Enter a number ");
-        num = numero_scanner.nextInt();
+        System.out.println(ingresar());
+    }
+    public static String ingresar() {
+        Scanner scankeyboard = new Scanner(System.in);
+        int num = scankeyboard.nextInt();
+        String result = enterNumbers(num);
+        return result;
+    }
+    public static String enterNumbers(int num) {
+        int adition = 0;
         if (num > 0) {
             for (int index = 0; index < num; index++) {
-                if (index % MODULOS_TRES == 0 || index % MODULO_CINCO == 0) {
+                if (index % THREE_MODULE == 0 || index % FIVE_MODULE == 0) {
                     System.out.println(index);
-                    suma += index;
+                    adition += index;
                 }
             }
-            System.out.println("the sum of the numbers is: " + suma);
+            return "the sum of the numbers is: " + adition;
         } else {
-            System.out.println("the sum of the numbers is: 0 ");
+            return "the sum of the numbers is: 0 ";
         }
     }
 }

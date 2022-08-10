7d9314a06cf175efa6199e31b8684184691dce4a
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-09 22:37:26 -0400

    Add test of multiples kata

--- src/main/java/jalau/at18/katas/multiples/daniela/Main.java
+++ src/main/java/jalau/at18/katas/multiples/daniela/Main.java
@@ -1,24 +1,24 @@
 package jalau.at18.katas.multiples.daniela;
-
+import java.util.Scanner;
 public class Main {
-    public static Integer myMultiple(int number) {
-        final int value = 0;
-        final int multiple3 = 3;
-        final int multiple5 = 5;
-        if (number < value) {
-            System.out.println("The number is not valid");
-        }
-        int total = value;
-        for (int find = value; find < number; find++) {
-            if (find % multiple3 == value || find % multiple5 == value) {
-                total += find;
+    static final Integer DIVIDE_1 = 3;
+    static final Integer DIVIDE_2 = 5;
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner scan = new Scanner(System.in);
+        Integer number = scan.nextInt();
+        Integer sumMultiples = getSumMult(number);
+        System.out.println("Sum: " + sumMultiples);
+    }
+    public static Integer getSumMult(Integer number) {
+        Integer sum = 0;
+        if (number >= 0) {
+            for (int find = 1; find < number; find++) {
+                if (find % DIVIDE_1 == 0 || find % DIVIDE_2 == 0) {
+                    sum = sum + find;
+                }
             }
         }
-        return total;
-    }
-
-    public static void main(String[] args) {
-        final int encontrar = 100;
-        myMultiple(encontrar);
+        return sum;
     }
 }

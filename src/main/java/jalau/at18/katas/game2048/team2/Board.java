9e14793d90851f40e3831a2552cd2364aedd520d
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-10 17:50:31 -0400

    Modify method generateTokens

--- src/main/java/jalau/at18/katas/game2048/team2/Board.java
+++ src/main/java/jalau/at18/katas/game2048/team2/Board.java
@@ -52,66 +52,47 @@ public class Board {
         }
 
     }
+
     public void generateTokens() {
         int[][] matriz = new int[4][4];
-        int cont=0;
-        int cont2=0;
+        int cont = 0;
+        int cont2 = 0;
         Random r = new Random();
+        int i1 = r.nextInt(matriz.length);
+        int i2 = r.nextInt(matriz.length);
+        System.out.print("Fila:" + i1 + ", ");
+        System.out.print("Columna:" + i2 + ": ");
+        // 2 pocicion
+        int i11 = r.nextInt(matriz.length);
+        int i22 = r.nextInt(matriz.length);
+        System.out.print("Fila:" + i11 + ", ");
+        System.out.print("Columna:" + i22 + ": ");
 
-            /// una posicion
-            int i1 = r.nextInt(matriz.length);
-            int i2 = r.nextInt(matriz.length);
-            System.out.print("Fila:" + i1 + ", ");
-            System.out.print("Columna:" + i2 + ": ");
-            //2 pocicion
-            int i11 = r.nextInt(matriz.length);
-            int i22 = r.nextInt(matriz.length);
-            System.out.print("Fila:" + i11 + ", ");
-            System.out.print("Columna:" + i22 + ": ");
+        System.out.println("--------------------------");
+        // int posicion1 = matriz[i1][i2];
+        // int posicion2 = matriz[i11][i22];
+        System.out.println(" posicion 1: " + matriz[i1][i11]);
+        System.out.println(" posicion 2: " + matriz[i2][i22]);
+        System.out.println("--------------------------");
 
-            System.out.println("--------------------------");
-          int posicion1= matriz[i1][i2];
-            int posicion2= matriz[i11][i22];
-            System.out.println(" posicion 1: " +matriz[i1][i11]);
-            System.out.println(" posicion 2: " +matriz[i2][i22]);
-            System.out.println("--------------------------");
-
-            int[][] matrizprincipal = new int[4][4];
-            for(int index =0 ; index <matrizprincipal[0].length; index++)
-            {
-                for(int indej =0 ; indej <matrizprincipal[1].length; indej++)
-                {
-                    if(cont == 0)
-                    {
-                        if (posicion1==matrizprincipal[index][indej])
-                            {
-                                matrizprincipal[index][indej]=2;
-                            cont=1;
-                            }
-
-                    }
-                    if(cont2 == 0)
-                    {
-                        if (posicion2==matrizprincipal[index][indej])
-                            {
-                                matrizprincipal[index][indej]=2;
-                            cont2=1;
-                            }
-
-                    }
-                    else{
-                        matrizprincipal[index][indej]=0;
-                    }
-                    
-                    System.out.print(matrizprincipal[index][indej]+" ");
+        int[][] matrizprincipal = new int[4][4];
+        for (int index = 0; index < matrizprincipal[0].length; index++) {
+            for (int indej = 0; indej < matrizprincipal[1].length; indej++) {
+                if (index == i1 && indej == i2) {
+                    matrizprincipal[index][indej] = 2;
                 }
-                System.out.println("");
-            }
 
+                if (index == i11 && indej == i22) {
+                    matrizprincipal[index][indej] = 2;
+                }
+                System.out.print(matrizprincipal[index][indej] + " ");
+            }
+            System.out.println("");
+        }
 
     }
-    public void replacePositions(int position1 , int Position2) {
-        
+
+    public void replacePositions(int position1, int Position2) {
 
     }
 

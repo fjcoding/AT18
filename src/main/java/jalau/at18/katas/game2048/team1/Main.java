c0c16d20d8aa1e4bb807233e59064dc0f19d1594
Maria Hurtado <Maria.Hurtado@jala.university>
2022-08-10 17:05:53 -0400

    Cleaning some comments

--- src/main/java/jalau/at18/katas/game2048/team1/Main.java
+++ src/main/java/jalau/at18/katas/game2048/team1/Main.java
@@ -2,18 +2,16 @@ package jalau.at18.katas.game2048.team1;
 
 import java.util.Random;
 
-public class Main  {
-
+public class Main {
+    //final int RANDOM_INITIAL_VALUES = 2;
     public static void main(String[] args) {
-        System.out.println("Empezando");
         /*
         for (int i = 0; i < 40; i++) {
             System.out.println(Arrays.toString(randomValuesGrid()));
         }
         */
-        int[] initialValues
-                = randomValuesGrid();
-        Integer[][] matrix = initialMatrix(initialValues[0],initialValues[1]);
+        int[] initialValues = randomValuesGrid();
+        Integer[][] matrix = initialMatrix(initialValues[0], initialValues[1]);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -21,17 +19,17 @@ public class Main  {
             System.out.println("");
         }
     }
-    public static int [] randomValuesGrid() {
-        int [] numbers = {2,4};
+
+    public static int[] randomValuesGrid() {
+        int[] numbers = {2, 4};
         Random random = new Random();
         int upperBound = 2; // utilizado para colocar el maximo valor, en nuestro caso la posicion de mi array
         int numberOne = random.nextInt(upperBound);
         int numberTwo = random.nextInt(upperBound);
-        System.out.println(numberOne + " - " + numberTwo);
         return new int[]{numbers[numberOne], numbers[numberTwo]};
     }
 
-    public static Integer[] randomPositionGrid () {
+    public static Integer[] randomPositionGrid() {
 
         Random rand = new Random(); //instance of random class
         int upperbound = 4; //generate random values from 0-4
@@ -45,17 +43,13 @@ public class Main  {
         Integer[][] matrix = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
         Integer[] firstPositionRowColum = randomPositionGrid();
         Integer[] secondPositionRowColum = randomPositionGrid();
-        System.out.println(firstPositionRowColum + ""+secondPositionRowColum);
-        //matrix = firstPositionRowColum
+        //System.out.println(firstPositionRowColum + ""+secondPositionRowColum);
 
-        while(firstPositionRowColum[0]  == secondPositionRowColum[0] && firstPositionRowColum[1]  == secondPositionRowColum[1]){
+        while (firstPositionRowColum[0]  == secondPositionRowColum[0] && firstPositionRowColum[1]  == secondPositionRowColum[1]){
             secondPositionRowColum = randomPositionGrid();
         }
 
-        int number1Row = firstPositionRowColum[0];
-        int number1Column = firstPositionRowColum[1];
-
-        matrix[number1Row][number1Column] = firstNumber;
+        matrix[firstPositionRowColum[0]][firstPositionRowColum[1]] = firstNumber;
         matrix[secondPositionRowColum[0] ][secondPositionRowColum[1]] = secondNumber;
 
         return matrix;

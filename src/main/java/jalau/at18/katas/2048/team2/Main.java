e29f47059d8f60dea513a6f8481169e295a215b1
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-10 09:11:06 -0400

    Add borad class

--- src/main/java/jalau/at18/katas/2048/team2/Main.java
+++ src/main/java/jalau/at18/katas/2048/team2/Main.java
@@ -1,5 +1,17 @@
 package jalau.at18.katas.2048.team2;
 
+
 public class Main {
-    
+
+	//2 clases una de jugador y de tablero
+    //Array de 2 dimensiones
+    //funcion de movimientos validos
+    //Casillas vacias y con eso limitar los movimientos
+    //Variable score
+    //Crear sumatoria de variables iguales
+    //Solo la posición es random y el numero aparece es siempre el 2
+}
+
+public class board (){
+
 }

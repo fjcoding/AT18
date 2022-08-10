f703a31692f32b6810771b500b9cb141d890b85f
Rene Rengel <Rene.Rengel@jala.university>
2022-08-10 18:44:26 -0400

    Add move logic

--- src/main/java/jalau/at18/katas/game2048/team4/Main.java
+++ src/main/java/jalau/at18/katas/game2048/team4/Main.java
@@ -5,39 +5,46 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static int[][] matrix = new int[4][4];
+    public static String nextDirection;
     public static void main(String[] args) {
         Scanner keyBoard = new Scanner(System.in);
         System.out.println("Bienvenido al juego");
         String exit = "Y";
-        String nextDirection;
         String continuePlaying;
+        int min = 0;
+        int max = 3;
+        int randomPositionX = ThreadLocalRandom.current().nextInt(min, max + 1);
+        int randomPositionY = ThreadLocalRandom.current().nextInt(min, max + 1);
+        System.out.println(randomPositionX);
+        System.out.println(randomPositionY);
+        matrix[randomPositionX][randomPositionY] = 2;
+        randomPositionX = ThreadLocalRandom.current().nextInt(min, max + 1);
+        randomPositionY = ThreadLocalRandom.current().nextInt(min, max + 1);
+        System.out.println(randomPositionX);
+        System.out.println(randomPositionY);
+        matrix[randomPositionX][randomPositionY] = 2;
+
+        showMatrix();
         
         do{
             
-            showMatrix();
             isWinner();
             
-          
-            
-            System.out.println("Type the direction you want to go \n up, left, right, down");
+            System.out.println("Type the direction you want to go \nDirections: up, left, right, down");
             nextDirection = keyBoard.nextLine();
+            directionChecker(nextDirection);
+            showMatrix();
             
-            System.out.println("If you want to stop playing type any exit if not type any key");
+            System.out.println("If you want to stop playing type exit \nif not type any key");
             continuePlaying = keyBoard.nextLine();
             if (continuePlaying.equals("exit")){
                 exit = "exit";     
             }
             
         } while(!isWinner() && exit != "exit"); //The game stops when the player type exit or wins
+
+
         keyBoard.close();
-        int min = 0;
-        int max = 3;
-        int randomPositionX = ThreadLocalRandom.current().nextInt(min, max + 1);
-        int randomPsotionY = ThreadLocalRandom.current().nextInt(min, max + 1);
-        //Just to check
-        System.out.println(randomPositionX); 
-        System.out.println(randomPsotionY);
-        System.out.println(matrix.length);
         
     }
 
@@ -58,6 +65,41 @@ public class Main {
             }
         }
         return false; 
+    }
+    static void directionChecker(String direction){
+        switch(direction) {
+            case "up":
+              System.out.println("uup");
+
+              break;
+            case "left":
+            System.out.println("leeeft");
+              break;
+            case "right":
+            System.out.println("riiight");
+              break;
+            case "down":
+            System.out.println("dooown");
+              break; 
+            default:
+              System.out.println("Type a correct direction");
+          }
+    }
+
+    static void moveNumbers(){
+        for (int index = 0; index < matrix.length; index++) {
+            for (int jIndex = 0; jIndex < matrix.length; jIndex++) {
+                if (matrix[index][jIndex] != 0 && index == 0){
+                    continue;
+                }
+                else if (matrix[index][jIndex] != 0 && matrix[index - 1][jIndex] == 0 ){
+                    int innerIndex = index; // movu up till reach a border or a number
+                    matrix[index - 1][jIndex] = matrix[index][jIndex];
+                    matrix[index][jIndex] = 0;
+                    
+                }
+            }
+        }
     }
 
 }

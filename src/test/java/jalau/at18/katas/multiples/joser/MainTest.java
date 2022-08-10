a404509423551fc9931ad724813ad5fddaba81bc
tonio <JoseRomay@jala.university>
2022-08-10 01:00:47 -0400

    Problem with TestMultiples, solved

--- src/test/java/jalau/at18/katas/multiples/joser/MainTest.java
+++ src/test/java/jalau/at18/katas/multiples/joser/MainTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class MainTest {
     public static final int PRIMERO = 10;
-    public static final int RESULTADO = 10;
+    public static final int RESULTADO = 23;
     @Test
     public void testMultiples() {
         Main main = new Main();

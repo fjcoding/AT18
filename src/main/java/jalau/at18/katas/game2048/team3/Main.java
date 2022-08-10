a58b0e58cb7436e4dbcfd52c7ae1e442e67a7f2f
tonio <JoseRomay@jala.university>
2022-08-10 16:52:48 -0400

    Add Function RightMove

--- src/main/java/jalau/at18/katas/game2048/team3/Main.java
+++ src/main/java/jalau/at18/katas/game2048/team3/Main.java
@@ -6,26 +6,43 @@ import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         // Scanner scan = new Scanner(System.in);
-        int[][] matrix = {{0, 0, 0, 2},
-                          {0, 0, 2, 2},
-                          {0, 0, 0, 0},
-                          {0, 0, 0, 0}};
+        int[][] matrix = {{2,0,2,0},
+                          {0,0,2,2},
+                          {2,2,2,2},
+                          {2,0,2,2}};
         // double randomNumber = Math.random()*1;
         // double [] randomNumbers = {2, 4};
-        showMatrix(matrix);
+        // showMatrix(matrix);
+        rightMove(matrix);
     }
 
     public static void rightMove(int[][] matrix) {
-        
-        // {2,0,2,0}
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j >= 0; j--) {
+                if(matrix[i][j]!=0){
+                    for (int j2 = j-1; j2 >=0; j2--) {
+                        if(matrix[i][j2]!=0 && matrix[i][j]==matrix[i][j2]){
+                            matrix[i][j]+=matrix[i][j2];
+                            matrix[i][j2]=0;
+                        } 
+                    }
+                }
+
+                // System.out.print(matrix[i][j]);
+            }
+            // System.out.println();
+            
+        }
+        showMatrix(matrix);
+        // {2,2,2,0}    // {0,0,4,0}   {0,0,2,4}    {4,2,0,0} 
         // {0,0,2,2}
-        // {2,2,2,2}
-        // {2,0,2,2}
+        // {2,2,2,2}    // {0,4,0,4} // {0,0,4,4}
+        // {2,0,2,2}    // {2,0,0,4} // {0,0,2,4}
 
         // {0,0,0,4}
-        // {0,0,2,2}
-        // {0,0,0,0}
-        // {0,0,0,0}
+        // {0,0,0,4}
+        // {0,0,4,4}
+        // {0,0,2,4}
     }
     public static void downMove(int[][] matrix) {
 

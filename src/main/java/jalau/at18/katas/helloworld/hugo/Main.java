cd92683581ebc3a9a232f5e6d7ebef62ac1d7357
Hugo Solares <Hugo.Solares@jala.university>
2022-08-10 08:18:22 -0400

    changing folders for the 5 exercises

---
+++ src/main/java/jalau/at18/katas/helloworld/hugo/Main.java
@@ -0,0 +1,12 @@
+package main.java.jalau.at18.katas.helloworld.hugo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Main().helloUser());
+    }
+
+    public String helloUser() {
+        return "Hello wuuuuurrld!!";
+    }
+}
---
+++ src/main/java/jalau/at18/katas/highestlowest/hugo/Main.java
@@ -0,0 +1,69 @@
+package main.java.jalau.at18.katas.highestlowest.hugo;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a string of numbers spaced with a blank space e.g: (1 2 -3 4 5)");
+        Scanner input = new Scanner(System.in);
+
+        // String inputNum = "1 2 3 4 5";
+        // String inputNum = "1 2 -3 4 5";
+        // String inputNum = "1 9 3 4 -5";
+
+        String inputNum = input.nextLine();
+        boolean noletter = true;
+
+        for (int k = 0; k < inputNum.length(); k++) {
+            char myletter = inputNum.charAt(k);
+
+            if (myletter == '1' || myletter == '2' || myletter == '3' || myletter == '4' || myletter == '5'
+                    || myletter == '6'
+                    || myletter == '7' || myletter == '8' || myletter == '9' || myletter == '0' || myletter == '-'
+                    || myletter == ' ') {
+                noletter = true;
+            } else {
+                noletter = false;
+                k = inputNum.length();
+                System.out.println("Letters are not allowed");
+            }
+        }
+
+        if (noletter == true) {
+
+            int max = 0;
+            int min = 0;
+
+            String[] concat = inputNum.split(" ");
+
+            int[] vector = new int[10];
+
+            for (int i = 0; i < concat.length; i++) {
+
+                vector[i] = Integer.parseInt(concat[i]);
+
+            }
+
+            max = vector[0];
+            min = vector[0];
+            for (int j = 0; j < concat.length; j++) {
+
+                if (vector[j] > max) {
+                    max = vector[j];
+                }
+
+                if (vector[j] < min) {
+                    min = vector[j];
+                }
+            }
+
+            System.out.println("Max = " + max);
+            System.out.println("Min = " + min);
+
+        }
+
+    }
+}

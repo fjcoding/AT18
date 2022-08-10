5afd14d531def9ee08323860b5947e4cd9bd2215
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-08-09 21:41:28 -0400

    modified code to english, restructured and unit test to highestlowest files

--- src/main/java/jalau/at18/katas/highestlowest/sarai/Main.java
+++ src/main/java/jalau/at18/katas/highestlowest/sarai/Main.java
@@ -3,26 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int cant;
-        Scanner scanner_teclado = new Scanner(System.in);
         System.out.println("how many numbers do you want to enter?");
-        cant = scanner_teclado.nextInt();
-        int[] alma = new int[cant];
+        System.out.println(enterNumbers());
+    }
+    public static String enterNumbers() {
+        Scanner scankeyboard = new Scanner(System.in);
+        int quantity = scankeyboard.nextInt();
+        int[] save = new int[quantity];
         System.out.println("enter numbers");
-        for (int index = 0; index < cant; index++) {
-            int num = scanner_teclado.nextInt();
-            alma[index] = num;
+        for (int index = 0; index < quantity; index++) {
+            int num = scankeyboard.nextInt();
+            save[index] = num;
         }
-        int numMayor = alma[0];
-        int numMenor = alma[0];
-        for (int index = 0; index < alma.length; index++) {
-            if (alma[index] > numMayor) {
-                numMayor = alma[index];
+        String message = highestLowestNumber(save);
+        return message;
+    }
+    public static String highestLowestNumber(int[] save) {
+        int highestNumber  = save[0];
+        int lowestNumber = save[0];
+        for (int index = 0; index < save.length; index++) {
+            if (save[index] > highestNumber) {
+                highestNumber  = save[index];
             }
-            if (alma[index] < numMenor) {
-                numMenor = alma[index];
+            if (save[index] < lowestNumber) {
+                lowestNumber = save[index];
             }
         }
-        System.out.println("numbers " + numMayor + " " + numMenor);
+        return "numbers " + " " + lowestNumber;
     }
 }

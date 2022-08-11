4128aaa8108d214850b400bdaca1c3987d960fff
Daniela Martinez <Daniela.Martinez@jala.university>
2022-08-11 09:00:00 -0400

    discomment to test the like file complete

--- src/main/java/jalau/at18/katas/likes/daniela/Like.java
+++ src/main/java/jalau/at18/katas/likes/daniela/Like.java
@@ -10,32 +10,33 @@ public class Like {
         final int quantity = 0;
         final int quantity4 = 4;
         String end = "complete execute";
+        String text = "";
         if (people > quantity) {
             int total_people = names.length;
             if (total_people == quantity1) {
-                return names[total_people - quantity] + " " + "like this";
+                text = text + " " + "like this";
             }
             if (total_people == quantity2) {
-                return names[total_people - quantity2] + " " + "and" + " "
+                text = names[total_people - quantity2] + " " + "and" + " "
                                 + names[total_people - quantity1] + " "
                                 + "likes this";
             }
             if (total_people == quantity3) {
-                return names[total_people - quantity3] + "," + " "
+                text = names[total_people - quantity3] + "," + " "
                         + names[total_people - quantity2] + " " + "and" + " " + names[total_people - quantity1]
                         + " " + "likes this";
             }
             if (total_people >= quantity4) {
                 int extra = total_people - 2;
-                return names[total_people - quantity4] + " " + "," + names[total_people - quantity3]
+                text = names[total_people - quantity4] + "," + " " + names[total_people - quantity3]
                                 + " " + "and" + " "
                                 + extra + " " + "others likes this";
             }
-            if (total_people == quantity) {
-                return "No body likes this";
+        else if (people == quantity) {
+                text = "No body likes this";
             }
         }
-        return end;
+        return text;
     }
 
     public static void main(String[] args) {
--- src/test/java/jalau/at18/katas/likes/daniela/LikesTest.java
+++ src/test/java/jalau/at18/katas/likes/daniela/LikesTest.java
@@ -11,21 +11,21 @@ public class LikesTest {
         String[] finalnamecase2 = {"Daniela", "Alejandra", "javier"};
         String[] finalnamecase3 = {"Daniela", "Alejandra"};
         //String[] finalnamecase4 = {"Daniela"};
-        //String[] finalnamecase5 = {};
+        String[] finalnamecase5 = {};
         String inputnames1 = like.myLikes(finalnamecase1);
         String inputnames2 = like.myLikes(finalnamecase2);
         String inputnames3 = like.myLikes(finalnamecase3);
         //String inputnames4 = like.myLikes(finalnamecase4);
-        //String inputnames5 = like.myLikes(finalnamecase5);
-        String output1 = "Daniela ,Alejandra and 2 others likes this";
+        String inputnames5 = like.myLikes(finalnamecase5);
+        String output1 = "Daniela, Alejandra and 2 others likes this";
         String output2 = "Daniela, Alejandra and javier likes this";
         String output3 = "Daniela and Alejandra likes this";
         //String output4 = "Daniela like this";
-        //String output5 = "No body likes this";
+        String output5 = "No body likes this";
         assertEquals(output1, inputnames1);
         assertEquals(output2, inputnames2);
         assertEquals(output3, inputnames3);
         //assertEquals(output4, inputnames4);
-        //assertEquals(output5, inputnames5);
+        assertEquals(output5, inputnames5);
     }
 }

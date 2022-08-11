867f1a31c9e97177881036d4270fb5247b95b178
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-10 22:26:08 -0400

    Add the the whole corrections from Jose

--- src/test/java/jalau/at18/katas/highestlowest/mauricio/MainTest.java
+++ src/test/java/jalau/at18/katas/highestlowest/mauricio/MainTest.java
@@ -7,20 +7,19 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class MainTest {
-    private static final int FIRST_NUMBER = -1;
-    private static final int SECOND_NUMBER = 34;
-    private static final int THIRD_NUMBER = 23;
-    private static final int FOURTH_NUMBER = -7;
-    private static final int FIFTH_NUMBER = 78;
+    private static final int FIRST_NUMBER_OF_ARRAY = -1;
+    private static final int SECOND_NUMBER_OF_ARRAY = 34;
+    private static final int THIRD_NUMBER_OF_ARRAY = 23;
+    private static final int FOURTH_NUMBER_OF_ARRAY = -7;
+    private static final int FIFTH_NUMBER_OF_ARRAY = 78;
 
     @Test
     public void returnHighestAndLowestValues() {
-        int[] input = {FIRST_NUMBER, SECOND_NUMBER, THIRD_NUMBER, FOURTH_NUMBER, FIFTH_NUMBER};
+        int[] input = {FIRST_NUMBER_OF_ARRAY, SECOND_NUMBER_OF_ARRAY, THIRD_NUMBER_OF_ARRAY, FOURTH_NUMBER_OF_ARRAY, FIFTH_NUMBER_OF_ARRAY};
         ArrayList<Integer> output = new ArrayList<>();
-        output.add(FOURTH_NUMBER);
-        output.add(FIFTH_NUMBER);
+        output.add(FOURTH_NUMBER_OF_ARRAY);
+        output.add(FIFTH_NUMBER_OF_ARRAY);
         Main main = new Main();
         assertEquals(output, main.phoneNumber(input));
-        // Se puede tener mas de un assert
     }
 }

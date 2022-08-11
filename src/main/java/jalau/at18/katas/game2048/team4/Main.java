4f332644b5582a2eba16ce6971242427bb19ffc6
Rene Rengel <Rene.Rengel@jala.university>
2022-08-11 12:09:31 -0400

    Add: putnNewTwoMethod, constants of bounds

--- src/main/java/jalau/at18/katas/game2048/team4/Main.java
+++ src/main/java/jalau/at18/katas/game2048/team4/Main.java
@@ -6,24 +6,23 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static int[][] matrix = new int[4][4];
     public static String nextDirection;
+    public static final int MIN_BOUND = 0;
+    public static final int MAX_BOUND = 3;
     public static void main(String[] args) {
         Scanner keyBoard = new Scanner(System.in);
         System.out.println("Bienvenido al juego");
         String exit = "Y";
         String continuePlaying;
-        int min = 0;
-        int max = 3;
-        int randomPositionX = ThreadLocalRandom.current().nextInt(min, max + 1);
-        int randomPositionY = ThreadLocalRandom.current().nextInt(min, max + 1);
-        System.out.println(randomPositionX);
-        System.out.println(randomPositionY);
-        matrix[randomPositionX][randomPositionY] = 2;
-        randomPositionX = ThreadLocalRandom.current().nextInt(min, max + 1);
-        randomPositionY = ThreadLocalRandom.current().nextInt(min, max + 1);
-        System.out.println(randomPositionX);
-        System.out.println(randomPositionY);
-        matrix[randomPositionX][randomPositionY] = 2;
+        
 
+        showMatrix();
+        System.out.println();
+        putNewTwoOnMatrix();
+        showMatrix();
+        System.out.println();
+        putNewTwoOnMatrix();
+        showMatrix();
+        System.out.println();
         showMatrix();
         
         do{
@@ -66,10 +65,12 @@ public class Main {
         }
         return false; 
     }
-    static void directionChecker(String direction){
+    static void directionChecker(String direction){ //
         switch(direction) {
             case "up":
               System.out.println("uup");
+              moveNumbersUp();
+              System.out.println();
 
               break;
             case "left":
@@ -86,21 +87,30 @@ public class Main {
           }
     }
 
-    static void moveNumbers(){
+    static void moveNumbersUp(){
         for (int index = 0; index < matrix.length; index++) {
             for (int jIndex = 0; jIndex < matrix.length; jIndex++) {
                 if (matrix[index][jIndex] != 0 && index == 0){
                     continue;
                 }
-                else if (matrix[index][jIndex] != 0 && matrix[index - 1][jIndex] == 0 ){
-                    int innerIndex = index; // movu up till reach a border or a number
-                    matrix[index - 1][jIndex] = matrix[index][jIndex];
-                    matrix[index][jIndex] = 0;
+                else if (matrix[index][jIndex] != 0 && matrix[index - 1][jIndex] == 0 ) {
+                    //for (int innerIndex = index; innerIndex >= 0 || matrix[innerIndex - 1][jIndex] != 0; innerIndex--) {
+                        matrix[index - 1][jIndex] = matrix[index][jIndex];
+                        matrix[index][jIndex] = 0;
+                    //} // movu up till reach a border or a number
+                   
                     
                 }
             }
         }
     }
+    static void putNewTwoOnMatrix() {
+        int positionX = ThreadLocalRandom.current().nextInt(MIN_BOUND, MAX_BOUND + 1);
+        int positionY = ThreadLocalRandom.current().nextInt(MIN_BOUND, MAX_BOUND + 1);
+        matrix[positionX][positionY] = 2;
+    }
+
+    
 
 }
 

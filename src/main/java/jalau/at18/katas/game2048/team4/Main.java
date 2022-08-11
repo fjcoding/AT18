4b53ee63e7c650d5affd52502a67e204ed276e5c
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-11 13:12:54 -0400

    New changes to Main.java

--- src/main/java/jalau/at18/katas/game2048/team4/Main.java
+++ src/main/java/jalau/at18/katas/game2048/team4/Main.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static int[][] matrix = new int[4][4];
     public static String nextDirection;
+
     public static void main(String[] args) {
         Scanner keyBoard = new Scanner(System.in);
         System.out.println("Bienvenido al juego");
@@ -25,82 +26,81 @@ public class Main {
         matrix[randomPositionX][randomPositionY] = 2;
 
         showMatrix();
-        
-        do{
-            
+
+        do {
+
             isWinner();
-            
+
             System.out.println("Type the direction you want to go \nDirections: up, left, right, down");
             nextDirection = keyBoard.nextLine();
             directionChecker(nextDirection);
             showMatrix();
-            
+
             System.out.println("If you want to stop playing type exit \nif not type any key");
             continuePlaying = keyBoard.nextLine();
-            if (continuePlaying.equals("exit")){
-                exit = "exit";     
+            if (continuePlaying.equals("exit")) {
+                exit = "exit";
             }
-            
-        } while(!isWinner() && exit != "exit"); //The game stops when the player type exit or wins
 
+        } while (!isWinner() && exit != "exit"); // The game stops when the player type exit or wins
 
         keyBoard.close();
-        
+
     }
 
-    static void showMatrix ( ){
+    static void showMatrix() {
         for (int index = 0; index < matrix.length; index++) {
             for (int jIndex = 0; jIndex < matrix.length; jIndex++) {
                 System.out.print(matrix[index][jIndex] + " ");
             }
-            System.out.println( );
+            System.out.println();
         }
     }
-    static boolean isWinner( ){
+
+    static boolean isWinner() {
         for (int index = 0; index < matrix.length; index++) {
             for (int jIndex = 0; jIndex < matrix.length; jIndex++) {
-                if (matrix[index][jIndex] == 2048){
+                if (matrix[index][jIndex] == 2048) {
                     return true;
                 }
             }
         }
-        return false; 
+        return false;
     }
-    static void directionChecker(String direction){
-        switch(direction) {
+
+    static void directionChecker(String direction) {
+        switch (direction) {
             case "up":
-              System.out.println("uup");
+                System.out.println("uup");
 
-              break;
+                break;
             case "left":
-            System.out.println("leeeft");
-              break;
+                System.out.println("leeeft");
+                break;
             case "right":
-            System.out.println("riiight");
-              break;
+                System.out.println("riiight");
+                break;
             case "down":
-            System.out.println("dooown");
-              break; 
+                System.out.println("dooown");
+                break;
             default:
-              System.out.println("Type a correct direction");
-          }
+                System.out.println("Type a correct direction");
+        }
     }
 
-    static void moveNumbers(){
+    static void moveNumbers() {
         for (int index = 0; index < matrix.length; index++) {
             for (int jIndex = 0; jIndex < matrix.length; jIndex++) {
-                if (matrix[index][jIndex] != 0 && index == 0){
+                if (matrix[index][jIndex] != 0 && index == 0) {
                     continue;
-                }
-                else if (matrix[index][jIndex] != 0 && matrix[index - 1][jIndex] == 0 ){
+                } else if (matrix[index][jIndex] != 0 && matrix[index - 1][jIndex] == 0) {
                     int innerIndex = index; // movu up till reach a border or a number
                     matrix[index - 1][jIndex] = matrix[index][jIndex];
                     matrix[index][jIndex] = 0;
-                    
+
                 }
             }
         }
     }
-
 }
 

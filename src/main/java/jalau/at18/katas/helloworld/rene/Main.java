148ffc1c3708b2a4a584a784bad5f7f482ec5a99
Rene Rengel <Rene.Rengel@jala.university>
2022-08-12 11:54:23 -0400

    Add unitTest for helloWorld file

--- src/main/java/jalau/at18/katas/helloworld/rene/Main.java
+++ src/main/java/jalau/at18/katas/helloworld/rene/Main.java
@@ -1,7 +1,9 @@
 package jalau.at18.katas.helloworld.rene;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        System.out.println(new Main().sayHello());
+    }
+    public String sayHello(){
+        return "Hello, World!";
     }
 }

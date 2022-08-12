1aed917dd2ab94ad13b314b8282b23fa592e5a45
Rene Rengel <Rene.Rengel@jala.university>
2022-08-12 13:32:43 -0400

    Fix checkstyle error on helloworld main file

--- src/main/java/jalau/at18/katas/helloworld/rene/Main.java
+++ src/main/java/jalau/at18/katas/helloworld/rene/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(new Main().sayHello());
     }
-    public String sayHello(){
+    public String sayHello() {
         return "Hello, World!";
     }
 }

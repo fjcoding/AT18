cd1f38ba19ec2eb9ca6cac74f9a8643f1377957f
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-12 17:04:34 -0400

    Add the first unittest to prove moveNumbersUp function

--- src/test/java/jalau/at18/katas/game2028/team4/MainTest.java
+++ src/test/java/jalau/at18/katas/game2028/team4/MainTest.java
@@ -9,7 +9,7 @@ public class MainTest {
     @Test
     public void moveNumbersUpTest() {
         int[][] matrix = { { 0, 0, 0, 0 }, { 0, 2, 2, 2 }, { 0, 4, 2, 8 }, { 2, 0, 0, 0 } };
-        int[][] expectedMatrix = { { 2, 2, 4, 2 }, { 0, 4, 0, 8 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        int[][] expectedMatrix = { { 2, 2, 2, 2 }, { 0, 4, 2, 8 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
         Main main = new Main();
         int[][] new_matrix = Main.moveNumbersUp(matrix);
         assertArrayEquals(expectedMatrix, new_matrix);

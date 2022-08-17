7c40e531e72ffb5d2dbfbbb337a424a7197dfeb5
Adriana Olivera <Adriana.Olivera@jala.university>
2022-08-17 02:07:39 -0400

    Create abstract class Tank and its principal abstract fuctions

---
+++ src/main/java/jalau/at18/battlecity/Tank.java
@@ -0,0 +1,9 @@
+package jalau.at18.battlecity;
+
+abstract class Tank {
+    abstract int[][] moveUp (int[][] matrix);
+    abstract int[][] moveDown (int[][] matrix);
+    abstract int[][] moveRight (int[][] matrix);
+    abstract int[][] moveLeft (int[][] matrix);
+    abstract void shoot ();
+}

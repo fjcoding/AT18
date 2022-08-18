3b1677471146f127310debb098540847d0f24d22
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-08-18 18:37:58 -0400

    Add validations for the tank player doesn't go out from the map

--- src/main/java/jalau/at18/battlecity/TankPlayer.java
+++ src/main/java/jalau/at18/battlecity/TankPlayer.java
@@ -5,7 +5,8 @@ public class TankPlayer extends Tank {
     private String direction;
     public static final int[][] INITIAL_POSITION = {{24, 9}, {24, 10}, {25, 9}, {25, 10}};
     public static final int SIZE_ROW_POSITION = 4;
-    private int[][] position = new int[SIZE_ROW_POSITION][2];
+    public static final int SIZE_MAX_BOARD = 26;
+    private int[][] newPosition = new int[SIZE_ROW_POSITION][2];
 
     public TankPlayer() {
 
@@ -13,49 +14,61 @@ public class TankPlayer extends Tank {
 
     @Override
     public int[][] initialPosition() {
-        this.position = INITIAL_POSITION;
-        return position;
+        this.newPosition = INITIAL_POSITION;
+        return newPosition;
     }
 
     public void setMove(String newDirection) {
         this.direction = newDirection;
     }
 
-    public void setPosition(int[][] newPosition) {
-        this.position = newPosition;
+    public void setPosition(int[][] position) {
+        this.newPosition = position;
 
     }
 
     @Override
-    public int[][] moveUp(int[][] newPosition) {
-        for (int row = 0; row < SIZE_ROW_POSITION; row++) {
-            newPosition[row][0] = newPosition[row][0] - 1;
+    public int[][] moveUp(int[][] position) {
+        newPosition = arrangeUp(position);
+        if (newPosition[0][0] !=0) {
+            for (int row = 0; row < SIZE_ROW_POSITION; row++) {
+                newPosition[row][0] = newPosition[row][0] - 1;
+            }
         }
         return newPosition;
     }
 
     @Override
-    public int[][] moveDown(int[][] newPosition) {
-        for (int row = 0; row < SIZE_ROW_POSITION; row++) {
-            newPosition[row][0] = newPosition[row][0] + 1;
+    public int[][] moveDown(int[][] position) {
+        newPosition = arrangeUp(position);
+        if (newPosition[0][0] != SIZE_MAX_BOARD - 1) {
+            for (int row = 0; row < SIZE_ROW_POSITION; row++) {
+                newPosition[row][0] = newPosition[row][0] + 1;
+            }
         }
         return newPosition;
     }
 
     @Override
-    public int[][] moveRight(int[][] newPosition) {
-        for (int row = 0; row < SIZE_ROW_POSITION; row++) {
-            newPosition[row][1] = newPosition[row][1] + 1;
+    public int[][] moveRight(int[][] position) {
+        newPosition = arrangeUp(position);
+        if (newPosition[0][1] != SIZE_MAX_BOARD - 1) {
+            for (int row = 0; row < SIZE_ROW_POSITION; row++) {
+                newPosition[row][1] = newPosition[row][1] + 1;
+            }
         }
         return newPosition;
     }
 
     @Override
-    public int[][] moveLeft(int[][] newPosition) {
-        for (int row = 0; row < SIZE_ROW_POSITION; row++) {
-            newPosition[row][1] = newPosition[row][1] - 1;
+    public int[][] moveLeft(int[][] position) {
+        newPosition = arrangeUp(position);
+        if (newPosition[0][1] != 0) {
+            for (int row = 0; row < SIZE_ROW_POSITION; row++) {
+                newPosition[row][1] = newPosition[row][1] - 1;
+            }
         }
-        return newPosition;
+       return newPosition;
     }
 
     @Override

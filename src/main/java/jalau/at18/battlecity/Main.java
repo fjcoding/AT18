fa2421ccf4bfd94962c5775a048ed2dc8c6d8e8e
Maria Hurtado <Maria.Hurtado@jala.university>
2022-08-18 19:34:27 -0400

    Updated Main class according to refactoring

--- src/main/java/jalau/at18/battlecity/Main.java
+++ src/main/java/jalau/at18/battlecity/Main.java
@@ -2,12 +2,12 @@ package jalau.at18.battlecity;
 
 public class Main {
     public static void main(String[] arg) {
-        CompleteBrick completeBrick = new CompleteBrick();
-        LeftBrick leftBrick = new LeftBrick();
-        LeftSteal leftSteal = new LeftSteal();
+        Brick completeBrick = new Brick();
+        Ice ice = new Ice();
+        Water water = new Water();
         Bushes bushes = new Bushes();
 
-        Element[][] matrix1 = {{completeBrick, leftBrick}, {leftSteal, bushes}};
+        Element[][] matrix1 = {{completeBrick, ice}, {water, bushes}};
 
         for (int row = 0; row < matrix1.length; row++) {
             for (int column = 0; column < matrix1.length; column++) {

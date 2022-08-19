6f3283aa67cf12f7a7d7a1bc5f5480bc2c9d6faa
Hugo Solares <Hugo.Solares@jala.university>
2022-08-19 01:04:21 -0400

    updating personal branch with the last changes in Spaceinvaders branch

---
+++ src/main/java/jalau/at18/spaceinvaders/Bullet.java
@@ -0,0 +1,12 @@
+package jalau.at18.spaceinvaders;
+
+public class Bullet {
+    
+    private int posX;
+    private int posY;
+
+    public Bullet(){
+        posX = 0;
+        posY = 0;
+    }
+}

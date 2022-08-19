afb8059d9b3e8fa5f090e41645772ba81256d0a2
Rene Rengel <Rene.Rengel@jala.university>
2022-08-19 14:33:14 -0400

    Fix all style errors

--- src/main/java/jalau/at18/pacman/Ghost.java
+++ src/main/java/jalau/at18/pacman/Ghost.java
@@ -2,19 +2,20 @@ package jalau.at18.pacman;
 
 public class Ghost {
     public static final String GHOST_CHAR = "G";
-   
+    public static final int INDEX_TRY = 5;
+    public static final int GHOST_SLEEP_TIME = 1000;
     private boolean isBlue = false;
     private String ghostRepresentation = GHOST_CHAR;
     private int[] position;
 
 
-    public void setIsBlue (Boolean blue) {
+    public void setIsBlue(Boolean blue) {
         this.isBlue = blue;
     }
     public boolean getIsBlue() {
         return this.isBlue;
     }
-    public void setPosition (int[] ghostPosition) {
+    public void setPosition(int[] ghostPosition) {
         this.position = ghostPosition;
     }
     public int[] getPosition() {
@@ -25,20 +26,19 @@ public class Ghost {
     }
 
     public void startGhostMovement() {
-        int i = 0;
-        while(i < 5) {
+        int index = 0;
+        while (index < INDEX_TRY) {
             System.out.println("skere");
             longRunningMethod();
-            i++;
+            index++;
         }
-       
     }
 
-     public static void longRunningMethod() {
+    public static void longRunningMethod() {
         try {
-           Thread.sleep(2000);
-        } catch (InterruptedException e) {
-           e.printStackTrace();
+            Thread.sleep(GHOST_SLEEP_TIME);
+        }   catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }

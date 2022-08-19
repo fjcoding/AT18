34e09128e91c0c6e49ced42a39a988986c10facb
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-18 22:00:59 -0400

    added unitetst for game class

---
+++ src/test/java/jalau/at18/spaceinvaders/GameTest.java
@@ -0,0 +1,26 @@
+package jalau.at18.spaceinvaders;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class GameTest {
+    @Test
+    public void shouldMoveTheShipToTheLeft() {
+        Game game =  new Game();
+        game.runGame('a');
+        assertEquals(3, game.getPosXShip());
+        assertEquals(9, game.getPosYShip());
+    }
+    @Test
+    public void shouldMoveTheShipToTheRight() {
+        Game game =  new Game();
+        game.runGame('d');
+        assertEquals(5, game.getPosXShip());
+        assertEquals(9, game.getPosYShip());
+    }
+   /*  @Test
+    public void itShouldReturnPositionShip() {
+        Game game =  new Game();
+        assertEquals(4, game.getPosXShip());
+        assertEquals(9, game.getPosYShip());
+    }
+    */
+}

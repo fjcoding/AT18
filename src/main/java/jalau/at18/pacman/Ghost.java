804914c6e9992223ecf4d2d3d9de3c0249b7815d
Rene Rengel <Rene.Rengel@jala.university>
2022-08-18 21:17:12 -0400

    Add attributes and methods to Ghost class

--- src/main/java/jalau/at18/pacman/Ghost.java
+++ src/main/java/jalau/at18/pacman/Ghost.java
@@ -1,5 +1,44 @@
 package jalau.at18.pacman;
 
 public class Ghost {
-    
+    public static final String GHOST_CHAR = "G";
+   
+    private boolean isBlue = false;
+    private String ghostRepresentation = GHOST_CHAR;
+    private int[] position;
+
+
+    public void setIsBlue (Boolean blue) {
+        this.isBlue = blue;
+    }
+    public boolean getIsBlue() {
+        return this.isBlue;
+    }
+    public void setPosition (int[] ghostPosition) {
+        this.position = ghostPosition;
+    }
+    public int[] getPosition() {
+        return this.position;
+    }
+    public String getRepresentation() {
+        return this.ghostRepresentation;
+    }
+
+    public void startGhostMovement() {
+        int i = 0;
+        while(i < 5) {
+            System.out.println("skere");
+            longRunningMethod();
+            i++;
+        }
+       
+    }
+
+     public static void longRunningMethod() {
+        try {
+           Thread.sleep(2000);
+        } catch (InterruptedException e) {
+           e.printStackTrace();
+        }
+    }
 }

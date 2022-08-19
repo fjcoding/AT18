0760f41a51f2b3925e485ff8e1611697eded0af0
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-18 22:00:21 -0400

    refactor to runGame function of Game.java and main.java and added function of get Position of ship in game.java

--- src/main/java/jalau/at18/spaceinvaders/Game.java
+++ src/main/java/jalau/at18/spaceinvaders/Game.java
@@ -1,5 +1,5 @@
 package jalau.at18.spaceinvaders;
-import java.util.Scanner;
+
 
 public class Game {
     private Ship ship;
@@ -7,38 +7,41 @@ public class Game {
     private Aliens alien;
     private static final int LIMIT_ROW = 4;
     private static final int LIMIT_COLUMN = 2;
-
+    private static final int SIZE_BOARD = 9;
     public Game() {
         board = new Board();
         ship = new Ship();
         alien = new Aliens();
     }
-    public void runGame() {
+
+    public void runGame(char instruction) {
         // print alien matrix
         for (int row = 0; row < LIMIT_ROW; row++) {
             for (int column = 0; column < LIMIT_COLUMN; column++) {
                 board.setElement(alien.getPosY()[column], alien.getPosX()[row], '%');
             }
         }
-        Scanner scanner = new Scanner(System.in);
-        // entrada de un carácter
-        while (true) {
-            char direction = scanner.next().charAt(0);
-            if (direction == 'a') {
-                ship.moveLeft();
-                board.setElement(ship.getPosY(), ship.getPosX(), '^');
-                System.out.println(board.toString());
-                board.setElement(ship.getPosY(), ship.getPosX(), '*');
-                //System.out.println(ship.getPosX());
-            }
-            if (direction == 'd') {
-                ship.moveRight();
-                //board.setElement(alien.getPosX(), alien.getPosY(), '%')
-                board.setElement(ship.getPosY(), ship.getPosX(), '^');
-                System.out.println(board.toString());
-                board.setElement(ship.getPosY(), ship.getPosX(), '*');
-                //System.out.println(ship.getPosX());
-            }
+        System.out.println(board.toString());
+        if (instruction == 'a' && ship.getPosX() > 0) {
+            ship.moveLeft();
+            board.setElement(ship.getPosY(), ship.getPosX(), '^');
+            board.setElement(ship.getPosY(), ship.getPosX() + 1, '*');
+        }
+        if (instruction == 'd' && ship.getPosX() < SIZE_BOARD) {
+            ship.moveRight();
+            board.setElement(ship.getPosY(), ship.getPosX(), '^');
+            board.setElement(ship.getPosY(), ship.getPosX() - 1, '*');
+        }
+        System.out.println(board.toString());
+        if (instruction == 'w') {
+            System.out.println("Fire");
         }
     }
+    public int getPosXShip() {
+        return ship.getPosX();
+    }
+    public int getPosYShip() {
+        return ship.getPosY();
+    }
 }
+

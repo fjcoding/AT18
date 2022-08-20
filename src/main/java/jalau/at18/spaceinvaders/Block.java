63edb64c9d7f12a335207287c72846a7284a55c9
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-20 10:54:01 -0400

    Add Block class in Spaceinvaders file

---
+++ src/main/java/jalau/at18/spaceinvaders/Block.java
@@ -0,0 +1,33 @@
+package jalau.at18.spaceinvaders;
+
+public class Block {
+    private static final int MAX_DURABILITY = 4;
+    private static final int THREE_DURABILITY = 3;
+    private static final int TWO_DURABILITY = 2;
+    private static final int UNICODE_CHAR = 126;
+    private int duration;
+    public Block() {
+        duration = MAX_DURABILITY;
+    }
+
+    public void impactBlock() {
+        duration--;
+    }
+
+    public char getCharacter() {
+        char res = '*';
+        if (duration == MAX_DURABILITY) {
+            res = '#';
+        }
+        if (duration == THREE_DURABILITY) {
+            res = '=';
+        }
+        if (duration == TWO_DURABILITY) {
+            res = UNICODE_CHAR;
+        }
+        if (duration == TWO_DURABILITY - 1) {
+            res = '-';
+        }
+        return res;
+    }
+}

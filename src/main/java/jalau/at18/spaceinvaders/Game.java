fe531bf677b437c32553556ee8b513f8412bb9ae
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-21 16:55:46 -0400

    Fixed ship death function in the game.java

--- src/main/java/jalau/at18/spaceinvaders/Game.java
+++ src/main/java/jalau/at18/spaceinvaders/Game.java
@@ -20,21 +20,20 @@ public class Game {
         int cont = 0;
         KeyboardReader keyboardObserver = new KeyboardReader();
         keyboardObserver.start();
-        //ship.setIsAlive();
         for (int lifes = 0; lifes < LIFES_SHIP; lifes++) {
+            board.setElement(ship.getPosY(), ship.getPosX(), '^');
+            System.out.println(board.toString());
+
             while (ship.getIsAlive()) {
                 if (cont % DELAY_ALIENS_MOVEMENT == 0) {
                     aliensList.alienMovement();
                 }
                 if (keyboardObserver.hasKeyEvents()) {
                     KeyEvent event = keyboardObserver.getEventFromTop();
-                    // If "left arrow", then move the game piece to the left
-                    // System.out.print(event.getKeyCode());
                     if (event.getKeyCode() == KeyEvent.VK_LEFT && ship.getPosX() > 0) {
                         ship.moveLeft();
                         board.setElement(ship.getPosY(), ship.getPosX(), '^');
                         board.setElement(ship.getPosY(), ship.getPosX() + 1, '*');
-                        // System.out.println(ship.getPosX());
                     } else if (event.getKeyCode() == KeyEvent.VK_RIGHT && ship.getPosX() < SIZE_BOARD) {
                         ship.moveRight();
                         board.setElement(ship.getPosY(), ship.getPosX(), '^');
@@ -44,21 +43,19 @@ public class Game {
                         System.out.println("fire");
                         ship.setIsDead();
                         ship.subtrasctLifes();
-                        System.out.println("muerio");
+                        board.setElement(ship.getPosY(), ship.getPosX(), '*');
+                        System.out.println("Died");
+                        ship.reStartShip();
                     }
 
                 }
                 System.out.println(board.toString());
-                //System.out.println(cont);
                 System.out.println("Lifes: " + ship.getLifes());
                 cont++;
                 Thread.sleep(DELAY);
 
             }
-        //System.out.println("Reiniciando juego");
             Thread.sleep(DELAY);
-            board.reStart();
-            aliensList.reStart();
             ship.setIsAlive();
         }
         System.out.println("Game Over");

20e61ccd25536d447b02de36ce3c3f162dca35d8
Adriana Olivera <Adriana.Olivera@jala.university>
2022-08-20 21:33:18 -0400

    Add enemy Tank to the board and it moves there

--- src/main/java/jalau/at18/battlecity/EnemyTank.java
+++ src/main/java/jalau/at18/battlecity/EnemyTank.java
@@ -1,24 +1,57 @@
 package jalau.at18.battlecity;
 
+import java.util.Random;
 
 public class EnemyTank extends Tank {
 
-    private String direction;
+    public String direction;
     public static final int GRID_LENGHT = 26;
     public static final int[][] INITIAL_POSITION_ONE = {{1, 1}, {1, 0}, {0, 1}, {0, 0}};
-    public static final int[][] INITIAL_POSITION_TWO = {{1, 14}, {1, 13}, {0, 14}, {0, 13}};
+    public static final int[][] INITIAL_POSITION_TWO = {{1, 13}, {1, 12}, {0, 13}, {0, 12}};
     public static final int[][] INITIAL_POSITION_THREE = {{1, 25}, {1, 24}, {0, 25}, {0, 24}};
     //public static final int ENEMY_FRONT = 10;
     //public static final int ENEMY_BACK = 11;
     public static final int INITIAL_POSITIONS = 3;
     public static final int POSITIONS_LENGTH = 4;
     private int enemyCount;
-    private int[][] enemyPosition;
+    public int[][] enemyPosition;
 
     public EnemyTank(int enemyCount) {
         this.enemyCount = enemyCount;
     }
 
+    public int[][] getPosition() {
+        return enemyPosition;
+    }
+    public String getDirection() {
+        return direction;
+    }
+    public void setDirection() {
+        String[] directions = {"UP", "DOWN", "LEFT", "RIGHT"};
+        Random random = new Random();
+        int upperBound = 4;
+        int randomDirection = random.nextInt(upperBound);
+        direction = directions[randomDirection];
+    }
+    public void goStraight() {
+        if (direction == "UP"){
+            enemyPosition = arrangeUp(enemyPosition);
+            enemyPosition = moveUp(enemyPosition);
+        }
+        if (direction == "DOWN"){
+            enemyPosition = arrangeDown(enemyPosition);
+            enemyPosition = moveDown(enemyPosition);
+
+        }
+        if (direction == "LEFT"){
+            enemyPosition = arrangeLeft(enemyPosition);
+            enemyPosition = moveLeft(enemyPosition);
+        }
+        if (direction == "RIGHT"){
+            enemyPosition = arrangeRight(enemyPosition);
+            enemyPosition = moveRight(enemyPosition);
+        }
+    }
     @Override
     public int[][] initialPosition() {
         if (enemyCount % INITIAL_POSITIONS == 1) {
@@ -30,12 +63,14 @@ public class EnemyTank extends Tank {
         if (enemyCount % INITIAL_POSITIONS == 0) {
             enemyPosition = INITIAL_POSITION_ONE;
         }
+        direction = "DOWN";
         return enemyPosition;
     }
 
     @Override
     public int[][] moveUp(int[][] position) {
         int[][] newPosition = arrangeUp(position);
+        direction = "UP";
         if (newPosition[0][0] != 0) {
             for (int row = 0; row < POSITIONS_LENGTH; row++) {
                 newPosition[row][0] = newPosition[row][0] - 1;
@@ -46,29 +81,32 @@ public class EnemyTank extends Tank {
 
     @Override
     public int[][] moveDown(int[][] position) {
-        int[][] newPosition = arrangeDown(position);
-        if (newPosition[0][0] != GRID_LENGHT - 1) {
+        //int[][] newPosition = arrangeDown(position);
+        direction = "DOWN";
+        if (position[0][0] != GRID_LENGHT - 1) {
             for (int row = 0; row < POSITIONS_LENGTH; row++) {
-                newPosition[row][0] = newPosition[row][0] + 1;
+                position[row][0] = position[row][0] + 1;
             }
         }
-        return newPosition;
+        return position;
     }
 
     @Override
     public int[][] moveLeft(int[][] position) {
-        int[][] newPosition = arrangeLeft(position);
-        if (newPosition[0][1] != 0) {
+        //int[][] newPosition = arrangeLeft(position);
+        direction = "LEFT";
+        if (position[0][1] != 0) {
             for (int row = 0; row < POSITIONS_LENGTH; row++) {
-                newPosition[row][1] = newPosition[row][1] - 1;
+                position[row][1] = position[row][1] - 1;
             }
         }
-        return newPosition;
+        return position;
     }
 
     @Override
     public int[][] moveRight(int[][] position) {
         int[][] newPosition = arrangeRight(position);
+        direction = "RIGHT";
         if (newPosition[0][1] != GRID_LENGHT - 1) {
             for (int row = 0; row < POSITIONS_LENGTH; row++) {
                 newPosition[row][1] = newPosition[row][1] + 1;
--- src/main/java/jalau/at18/battlecity/GameBattlecity.java
+++ src/main/java/jalau/at18/battlecity/GameBattlecity.java
@@ -3,16 +3,17 @@ package jalau.at18.battlecity;
 public class GameBattlecity extends Game {
     public int enemyCount = 20;
     private TankPlayer tankPlayer = new TankPlayer();
+    public EnemyTank enemyTank = new EnemyTank(enemyCount);
     public static final int SIZE_ROW_POSITION = 4;
-
+    public Board board;
     public GameBattlecity() {
-
+    
     }
 
     public void startGame() {
-        Board board = createBoard();
-        TankPlayer tankPlayer = new TankPlayer();
-        EnemyTank enemyTank = new EnemyTank(enemyCount);
+        this.board = createBoard();
+        //TankPlayer tankPlayer = new TankPlayer();
+        //EnemyTank enemyTank = new EnemyTank(enemyCount);
         Element[][] elementsMatrix = board.getMatrix();
         int[][] positionPlayer = tankPlayer.initialPosition();
         int[][] positionEnemy = enemyTank.initialPosition();
@@ -21,17 +22,29 @@ public class GameBattlecity extends Game {
         showMatrix(board);
     }
     public void playGame() {
-        Board board = createBoard();
-        Input direction = new Input();
-        String input = direction.read();
-        int[][] newPosition = new int[SIZE_ROW_POSITION][2];
-        newPosition = tankPlayer.movements(input);
-        Element[][] elementsMatrix = board.getMatrix();
-        elementsMatrix = tankPlayer.putTankOnBoard(elementsMatrix, newPosition);
-        showMatrix(board);
+        //Board board = createBoard();
+        //Input direction = new Input();
+        //String input = direction.read();
+        //int[][] newPosition = new int[SIZE_ROW_POSITION][2];
+        //newPosition = tankPlayer.movements(input);
+        //Element[][] elementsMatrix = board.getMatrix();
+        //elementsMatrix = tankPlayer.putTankOnBoard(elementsMatrix, newPosition);
+        moveEnemy();
+        
     }
     public void endGame() {
 
+    }
+    public void moveEnemy() {
+        for (int movementCount = 0; movementCount < 10; movementCount++) {
+            Element[][] elementsMatrix = board.getMatrix();
+            enemyTank.goStraight();
+            System.out.println(enemyTank.getDirection());
+            elementsMatrix = enemyTank.putTankOnBoard(elementsMatrix, enemyTank.getPosition());
+            showMatrix(board);
+            wait(1);
+        }
+        enemyTank.setDirection();     
     }
     public static Board createBoard() {
         String rute = "stage1.csv";
@@ -40,10 +53,6 @@ public class GameBattlecity extends Game {
         return board;
     }
     public static void showMatrix(Board board) {
-        Brick brick = new Brick();
-        Steel steel = new Steel();
-        Bushes bushes = new Bushes();
-
         Element[][] matrix = board.getMatrix();
 
         for (int row = 0; row < matrix.length; row++) {
@@ -53,5 +62,12 @@ public class GameBattlecity extends Game {
             System.out.println();
         }
 
+    }
+    public static void wait(int seconds){
+        try {
+            Thread.sleep(seconds * 1000);
+         } catch (Exception e) {
+            System.out.println(e);
+         }
     }
 }

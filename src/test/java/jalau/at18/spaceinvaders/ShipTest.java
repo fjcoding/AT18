d48e89d7ff4619f2b3abf26e107289dce3a39769
Alvaro Sivila <alvaro.sivila@jala.university>
2022-08-21 19:22:14 -0400

    Added unittest to ship

--- src/test/java/jalau/at18/spaceinvaders/ShipTest.java
+++ src/test/java/jalau/at18/spaceinvaders/ShipTest.java
@@ -35,4 +35,41 @@ public class ShipTest {
         Ship ship = new Ship();
         assertEquals(9, ship.getPosY());
     }
+    @Test
+    public void itShouldReturnStatusOfLifes() {
+        Ship ship = new Ship();
+        assertEquals(true, ship.getIsAlive());
+
+    }
+    @Test
+    public void itShouldChangeStatusToDeathOfShip() {
+        Ship ship = new Ship();
+        ship.setIsDead();
+        assertEquals(false, ship.getIsAlive());
+
+    }
+    @Test
+    public void itShouldChangeStatusToLiveOfShip() {
+        Ship ship = new Ship();
+        ship.setIsDead();
+        ship.setIsAlive();
+        assertEquals(true, ship.getIsAlive());
+
+    }
+    @Test
+    public void itShouldRestartPosition() {
+        Ship ship = new Ship();
+        ship.moveRight();
+        ship.reStartShip();
+        assertEquals(9,ship.getPosY());
+        assertEquals(4,ship.getPosX());
+
+    }
+    @Test
+    public void itShouldSubstractOneLife() {
+        Ship ship = new Ship();
+        ship.subtrasctLifes();
+        assertEquals(2,ship.getLifes());
+
+    }
 }

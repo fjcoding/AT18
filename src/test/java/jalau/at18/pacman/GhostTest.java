1c5aecb3700f2ad0ddb7ab5e7830d4048c34b13b
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-21 23:49:15 -0400

    New Unittest on GhostTest file

--- src/test/java/jalau/at18/pacman/GhostTest.java
+++ src/test/java/jalau/at18/pacman/GhostTest.java
@@ -3,26 +3,26 @@ package jalau.at18.pacman;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class ConditionMovementsGhostsTest {
-    ConditionMovementsGhosts movementG = new ConditionMovementsGhosts();
+public class GhostTest {
+    Ghost movements = new Ghost();
     Board board = new Board();
     private String[][] arr = board.getGameBoard();
     final int rowPacman = 1;
     final int columsPacman = 1;
     private static int rowGhost = 8;
     private static int columnGhost = 15;
-    char a = 'a';
-    char s = 's';
-    char w = 'w';
-    char d = 'd';
+    public static char[] ghostMovements = { 'w', 'a', 's', 'd' };
+    char r;
 
     @Test
     public void testMovement() {
         arr[rowPacman][columsPacman] = "\033[33mC\u001B[0m";
         arr[rowGhost][columnGhost] = "\033[31mG\u001B[0m";
-        String[][] moveUpGhost = movementG.movement(w, arr);
-        board.setGameBoard(moveUpGhost);
+        String[][] moveGhost = movements.ghostMovement(arr);
+        board.setGameBoard(moveGhost);
         String[][] arr_2 = board.getGameBoard();
-        assertEquals(arr[rowGhost][columnGhost], arr_2[rowGhost][columnGhost]);
+        assertEquals(arr[rowPacman][columsPacman], arr_2[rowPacman][columsPacman]);
+
     }
+
 }

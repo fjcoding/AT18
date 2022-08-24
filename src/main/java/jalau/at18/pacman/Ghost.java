ef8427e67cd0aaf2920204c9757d466d7c8adb90
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-08-24 15:48:00 -0400

    Corrections on Checkstyle from Ghost.java

--- src/main/java/jalau/at18/pacman/Ghost.java
+++ src/main/java/jalau/at18/pacman/Ghost.java
@@ -3,53 +3,12 @@ package jalau.at18.pacman;
 import java.util.List;
 
 public class Ghost {
-    public static final String GHOST_CHAR = "\033[31mG\u001B[0m";
     public static final int INDEX_TRY = 5;
     public static final int FOUR_MOVEMENTS = 4;
     public static final int GHOST_SLEEP_TIME = 1000;
-    private boolean isBlue = false;
-    private String ghostRepresentation = GHOST_CHAR;
-    private int[] position;
-    private static final char[] GHOST_MOVEMENTS = { 'w', 'a', 's', 'd' }; // public
+    private static final char[] GHOST_MOVEMENTS = {'w', 'a', 's', 'd'};
     private static final ConditionMovementsGhosts MOVEMENTS = new ConditionMovementsGhosts();
     private int random;
-
-    public void setIsBlue(Boolean blue) {
-        this.isBlue = blue;
-    }
-
-    public boolean getIsBlue() {
-        return this.isBlue;
-    }
-
-    public void setPosition(int[] ghostPosition) {
-        this.position = ghostPosition;
-    }
-
-    public int[] getPosition() {
-        return this.position;
-    }
-
-    public String getRepresentation() {
-        return this.ghostRepresentation;
-    }
-
-    public void startGhostMovement() {
-        int index = 0;
-        while (index < INDEX_TRY) {
-            System.out.println("skere");
-            longRunningMethod();
-            index++;
-        }
-    }
-
-    public static void longRunningMethod() {
-        try {
-            Thread.sleep(GHOST_SLEEP_TIME);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     public List<Object> ghostMovement(String[][] array, int rowGhost, int columnsGhost) {
         random = (int) (Math.random() * FOUR_MOVEMENTS);

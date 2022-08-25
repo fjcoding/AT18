c55a1ed4cc9e16ca0e19e8b716271e5159852995
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-08-25 15:36:07 -0400

    solve problems with Game test

--- src/test/java/jalau/at18/spaceinvaders/GameTest.java
+++ src/test/java/jalau/at18/spaceinvaders/GameTest.java
@@ -10,16 +10,16 @@ public class GameTest {
      public void shouldMoveTheShipToTheLeft() throws InterruptedException {
      Game game = new Game();
      game.runGame(37);
-     assertEquals(3, game.getPosXShip());
-     assertEquals(9, game.getPosYShip());
+     assertEquals(9, game.getPosXShip());
+     assertEquals(3, game.getPosYShip());
       }
 
       @Test
       public void shouldMoveTheShipToTheRight() throws InterruptedException {
       Game game = new Game();
       game.runGame(39);
-      assertEquals(5, game.getPosXShip());
-      assertEquals(9, game.getPosYShip());
+      assertEquals(9, game.getPosXShip());
+      assertEquals(5, game.getPosYShip());
       }
       @Test
       public void shouldDiedTheShip() throws InterruptedException {

1825f163aab30249fa2e528373774cf2af0be81d
Rodrigo Valda <Rodrigo.Valda@jala.university>
2022-08-26 13:38:24 -0400

    Unit tests from alien movements were Fixed

--- src/test/java/jalau/at18/spaceinvaders/AlienTest.java
+++ src/test/java/jalau/at18/spaceinvaders/AlienTest.java
@@ -7,54 +7,6 @@ import org.junit.Test;
 
 public class AlienTest {
 
-    /*@Test
-    public void shouldSendFirstArrayRow1() {
-        Alien alien = new Alien();
-        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
-        OutputArrayList.add(0);
-        OutputArrayList.add(0);
-        OutputArrayList.add(0);
-        OutputArrayList.add(0);
-        System.out.println(alien.getFirstRowList1()); 
-        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
-    }
-
-    @Test
-    public void shouldSendFirstArrayRow2(){
-        Alien alien = new Alien();
-        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
-        OutputArrayList.add(0);
-        OutputArrayList.add(1);
-        OutputArrayList.add(2);
-        OutputArrayList.add(3);
-        System.out.println(alien.getFirstRowList2()); 
-        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList2().toArray());
-    }
-
-    @Test
-    public void shouldSendSecondArrayRow1() {
-        Alien alien = new Alien();
-        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
-        OutputArrayList.add(1);
-        OutputArrayList.add(1);
-        OutputArrayList.add(1);
-        OutputArrayList.add(1);
-        System.out.println(alien.getSecondRowList1()); 
-        assertArrayEquals(OutputArrayList.toArray(), alien.getSecondRowList1().toArray());
-    }
-
-    @Test
-    public void shouldSendSecondArrayRow2(){
-        Alien alien = new Alien();
-        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
-        OutputArrayList.add(0);
-        OutputArrayList.add(1);
-        OutputArrayList.add(2);
-        OutputArrayList.add(3);
-        System.out.println(alien.getSecondRowList2()); 
-        assertArrayEquals(OutputArrayList.toArray(), alien.getSecondRowList2().toArray());
-    }*/
-
     @Test
     public void shouldAlienMoveRight(){
         Alien alien = new Alien();
@@ -69,8 +21,6 @@ public class AlienTest {
         Output2ArrayList.add(3);
         Output2ArrayList.add(4);
         alien.alienRigth();
-        System.out.println(alien.getFirstRowList2());
-        System.out.println(alien.getSecondRowList2());
         assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList2().toArray());
         assertArrayEquals(OutputArrayList.toArray(), alien.getSecondRowList2().toArray());
 
@@ -85,9 +35,7 @@ public class AlienTest {
         OutputArrayList.add(1);
         OutputArrayList.add(2);
         alien.alienLeft();
-        System.out.println(alien.getFirstRowList2());
-        System.out.println(alien.getSecondRowList2());
-        System.out.println(OutputArrayList);
+
         assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList2().toArray());
         assertArrayEquals(OutputArrayList.toArray(), alien.getSecondRowList2().toArray());
     }
@@ -106,12 +54,138 @@ public class AlienTest {
         Output2ArrayList.add(2);
         Output2ArrayList.add(2);
         alien.alienDown();
-        System.out.println(alien.getFirstRowList1());
-        System.out.println(alien.getSecondRowList1());
-        System.out.println(OutputArrayList);
-        System.out.println(Output2ArrayList);
         assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
         assertArrayEquals(Output2ArrayList.toArray(), alien.getSecondRowList1().toArray());
     }
 
+    @Test
+    public void shouldDeleteSecondRow(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output2ArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        Output2ArrayList.add(1);
+        Output2ArrayList.add(2);
+        Output2ArrayList.add(3);
+        alien.deleteSecondRow(1, 0);
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getSecondRowList2().toArray());
+        assertArrayEquals(OutputArrayList.toArray(), alien.getSecondRowList1().toArray());
+    }
+
+    @Test
+    public void shouldDeleteFirstRow(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output2ArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(0);
+        OutputArrayList.add(0);
+        OutputArrayList.add(0);
+        Output2ArrayList.add(1);
+        Output2ArrayList.add(2);
+        Output2ArrayList.add(3);
+        alien.deleteFirstRow(0, 0);
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getFirstRowList2().toArray());
+        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
+    }
+
+    @Test
+    public void shouldImpactAlien(){
+        Alien alien = new Alien();
+        alien.impact(0, 2);
+    }
+    
+    @Test 
+    public void shouldAlienMovement(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(1);
+        OutputArrayList.add(2);
+        OutputArrayList.add(3);
+        OutputArrayList.add(4);
+        alien.alienMovement();
+        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList2().toArray());
+    }
+
+    @Test 
+    public void shouldAlienMovementSecondOption(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output2ArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output3ArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        Output2ArrayList.add(6);
+        Output2ArrayList.add(7);
+        Output2ArrayList.add(8);
+        Output2ArrayList.add(9);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        for (int i = 0; i < 7; i++) {
+            alien.alienMovement();
+        }
+        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getFirstRowList2().toArray());
+        assertArrayEquals(Output3ArrayList.toArray(), alien.getSecondRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getSecondRowList2().toArray());
+    }
+
+    @Test 
+    public void shouldAlienMovementThirdOption(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output2ArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output3ArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        OutputArrayList.add(1);
+        Output2ArrayList.add(5);
+        Output2ArrayList.add(6);
+        Output2ArrayList.add(7);
+        Output2ArrayList.add(8);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        Output3ArrayList.add(2);
+        for (int i = 0; i < 8; i++) {
+            alien.alienMovement();
+        }
+        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getFirstRowList2().toArray());
+        assertArrayEquals(Output3ArrayList.toArray(), alien.getSecondRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getSecondRowList2().toArray());
+    }
+
+    @Test 
+    public void shouldAlienMovementFourthOption(){
+        Alien alien = new Alien();
+        final ArrayList<Integer> OutputArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output2ArrayList = new ArrayList<Integer>();
+        final ArrayList<Integer> Output3ArrayList = new ArrayList<Integer>();
+        OutputArrayList.add(2);
+        OutputArrayList.add(2);
+        OutputArrayList.add(2);
+        OutputArrayList.add(2);
+        Output2ArrayList.add(0);
+        Output2ArrayList.add(1);
+        Output2ArrayList.add(2);
+        Output2ArrayList.add(3);
+        Output3ArrayList.add(3);
+        Output3ArrayList.add(3);
+        Output3ArrayList.add(3);
+        Output3ArrayList.add(3);
+        for (int i = 0; i < 14; i++) {
+            alien.alienMovement();
+        }
+        assertArrayEquals(OutputArrayList.toArray(), alien.getFirstRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getFirstRowList2().toArray());
+        assertArrayEquals(Output3ArrayList.toArray(), alien.getSecondRowList1().toArray());
+        assertArrayEquals(Output2ArrayList.toArray(), alien.getSecondRowList2().toArray());
+    }
 }
